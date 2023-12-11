28b9380a977489d2e46a693c40139ecb80891f16
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2023-12-11 10:08:36 +0000

    Update Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,21 @@
 {
     "presets": [
-        ["@babel/preset-env", {
-            "targets": "> 0.25%, not dead"
-        }],
+        "@babel/preset-env",
+            {
+                "targets": "> 0.25%, not dead"
+            },
         "@babel/preset-react",
-        ["@babel/preset-typescript",{
-            "allExtensions": true,
-            "isTSX": true
-        }]
+        "@babel/preset-typescript",
+            {
+                "allExtensions": true,
+                "isTSX": true
+            }
     ],
-    "plugins": [
+    "plugins"; [
         "@babel/preset-typescript",
         "@babel/core",
         "@babel/eslint-parser",
         "@babel/plugin-proposal-private-property-in-object",
         "@babel/preset-env"
-    ];
+    ]
 }
