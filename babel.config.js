8740a8be33af2ef4a3f2ecd867f48c8a346be43f
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2023-12-11 09:59:44 +0000

    Update Babel configuration to include presets and plugins
    This commit updates the Babel configuration file (babel.config.js) to include additional presets and plugins. The presets added are "@babel/preset-react" and "@babel/preset-typescript", with specific options for TypeScript. The plugins added are "@babel/preset-typescript", "@babel/core", "@babel/eslint-parser", "@babel/plugin-proposal-private-property-in-object", and "@babel/preset-env".

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
 {
     "presets": [
-      ["@babel/preset-env", {
-        "targets": "> 0.25%, not dead"
-      }]
-    ]
-  }
+        ["@babel/preset-env", {
+            "targets": "> 0.25%, not dead"
+        }],
+        "@babel/preset-react",
+        ["@babel/preset-typescript",{
+            "allExtensions": true,
+            "isTSX": true
+        }]
+    ],
+    "plugins": [
+        "@babel/preset-typescript",
+        "@babel/core",
+        "@babel/eslint-parser",
+        "@babel/plugin-proposal-private-property-in-object",
+        "@babel/preset-env"
+    ];
+}
