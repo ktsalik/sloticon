354dda28abc49a93b64ad8931a617a19af0f2fe0
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2024-03-21 03:56:27 -0500

    Update socket.js

--- src/context/socket.js
+++ src/context/socket.js
@@ -3,7 +3,7 @@ import React from 'react';
 import store from '../store';
 import lobbySlice from "../lobbySlice";
 
-export const socket = io.connect('https://sloticon.onrender.com');
+export const socket = io.connect('https://royalgames-server.onrender.com');
 // export const socket = io.connect('http://localhost:3001');
 export const SocketContext = React.createContext();
 
